c1062d2ca25d93b5331a788cea1444ae5eb1baff
Tim <tim.samuelsen@gmail.com>
2024-04-21 16:28:24 -0700

    starting

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use leptos::*;
+
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+
+    view! {
+        <button
+            on:click = move |_| {
+                set_count.update(|n| *n += 1);
+            }
+        >
+            "Click me: "
+            {move || count.get()}
+        </button>
+    }
+}
+
+fn main() {
+    mount_to_body(|| view! { <App/> })
+}

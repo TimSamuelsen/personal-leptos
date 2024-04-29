acbedc352037e5aba4196aaa78af3e5ec9d83a33
Tim <tim.samuelsen@gmail.com>
2024-04-28 21:50:42 -0700

    starting to come along

--- src/postcard.rs
+++ src/postcard.rs
@@ -2,28 +2,27 @@
 use leptos::*;
 
 pub fn Projects() -> impl IntoView {
+  let experiences = vec![
+        PostMetadata {
+          image_path: String::from("/public/carbon_backpack_crop.jpg"),
+          title: String::from("backypacky"),
+          project_link: String::from("https://www.carbon3d.com/products/ao-suite-hardware"),
+        },
+        PostMetadata {
+          image_path: "/public/sciene_cover_iclip.jpg".to_string(),
+          title: "me".to_string(),
+          project_link: "https://www.science.org/doi/10.1126/sciadv.abq3917".to_string(),
+        },
+        PostMetadata {
+          image_path: "/public/github.svg".to_string(),
+          title: "Patent".to_string(),
+          project_link: "www.google.com".to_string(),
+        }
+    ];
+
   view! {
-    <div class="grid sm:grid-cols-2 lg:grid-cols-2 gap-8">
-                <PostCard
-                  post_metadata= PostMetadata {
-                    image_path: "/public/stanford.svg".to_string(),
-                    title: "me".to_string(),
-                    date: "01/01/01".to_string(),
-                    description: "test".to_string(),
-                    project_link: "www.google.com".to_string(),
-                  }
-                  path="test".to_string()
-                />
-                <PostCard
-                  post_metadata= PostMetadata {
-                    image_path: "/public/stanford.svg".to_string(),
-                    title: "Patent".to_string(),
-                    date: "01/01/01".to_string(),
-                    description: "test".to_string(),
-                    project_link: "www.google.com".to_string(),
-                  }
-                  path="test".to_string()
-                />
+    <div class="grid sm:grid-cols-3 lg:grid-cols-3 gap-6">
+    {experiences.into_iter().map(|post_metadata| (PostCardProps { post_metadata: post_metadata })).collect::<Vec<_>>()}
     </div>
   }
 
@@ -33,32 +32,24 @@ pub fn Projects() -> impl IntoView {
 pub struct PostMetadata {
   pub image_path: String,
   pub title: String,
-  pub date: String,
-  pub description: String,
   pub project_link: String,
 }
 
 #[component]
-pub fn PostCard(post_metadata: PostMetadata, path: String) -> impl IntoView {
+pub fn PostCard(post_metadata: PostMetadata) -> impl IntoView {
     view! {
         <a
-            class="group flex flex-col h-full border transition-all duration-300 rounded-xl p-5 border-gray-700 hover:border-transparent hover:shadow-black/[.4]"
-            href=format!("/{}/{}", path, "post_metadata".to_string())
+          class="group flex flex-col h-full border transition-all duration-300 p-5 border-gray-700 hover:shadow-black/[.4] grayscale hover:grayscale-0"
+          href=post_metadata.project_link
         >
-            <div class="aspect-w-16 aspect-h-11">
-                <img class="w-full object-cover rounded-xl" src=post_metadata.image_path/>
-            </div>
-            <div class="my-6">
-                <h3 class="text-xl font-semibold  text-gray-300 group-hover:text-[#F8F9FA]">
-                    {post_metadata.title}
-                </h3>
-                <h2 class="mt-5 text-gray-400">{post_metadata.date}</h2>
-                <p class="mt-5 text-[#CED4DA]">{post_metadata.description}</p>
-            </div>
-            <div class="mt-auto flex items-center gap-x-3">
-                <img class="w-8 h-8 rounded-full" src="https://github.com/itehax.png"/>
-                <h5 class="text-sm text-gray-200">"By Itehax."</h5>
-            </div>
+          <div class="aspect-w-11 aspect-h-11">
+              <img class="w-full object-cover rounded-xl" src=post_metadata.image_path/>
+          </div>
+          <div class="my-6">
+              <h3 class="text-xl font-semibold  text-gray-300 group-hover:text-[#F8F9FA]">
+                  {post_metadata.title}
+              </h3>
+          </div>
         </a>
     }
 }

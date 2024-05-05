0f2c496b98c55d571cd69560cbc981a7ee8bc905
Tim <tim.samuelsen@gmail.com>
2024-05-05 15:55:38 -0700

    redo route

--- src/app.rs
+++ src/app.rs
@@ -1,18 +1,18 @@
 use leptos::*;
-//use leptos_router::*;
+use leptos_router::*;
 
 use crate::home::Home;
 
 #[component]
 pub fn App() -> impl IntoView {
     view! {
-        <Home/>
+        //<Home/>
         //<Stylesheet id="leptos" href="style/output.css"/>
         //<Link rel="shortcut icon" type_="image/ico" href="/favicon.ico"/>
-        // <Router>
-        //     <Routes>
-        //         <Route path="" view=  move || view! { <Home/> }/>
-        //     </Routes>
-        // </Router>
+        <Router>
+            <Routes>
+                <Route path="" view=  move || view! { <Home/> }/>
+            </Routes>
+        </Router>
     }
 }

6763fa88cb77b914b79da852a00c94b74077a2dc
Tim <tim.samuelsen@gmail.com>
2024-05-05 16:07:30 -0700

    no weak refs?

--- src/app.rs
+++ src/app.rs
@@ -6,9 +6,6 @@ use crate::home::Home;
 #[component]
 pub fn App() -> impl IntoView {
     view! {
-        //<Home/>
-        //<Stylesheet id="leptos" href="style/output.css"/>
-        //<Link rel="shortcut icon" type_="image/ico" href="/favicon.ico"/>
         <Router>
             <Routes>
                 <Route path="" view=  move || view! { <Home/> }/>
